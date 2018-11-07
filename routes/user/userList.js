d5ad8a7253a4ad29a9fcefe8c9184a53769e92fd
dyh-sjtu <dyh_sjtu@163.com>
2018-11-07 21:07:48 +0800

    增加修改密码和个人中心

--- routes/user/userList.js
+++ routes/user/userList.js
@@ -60,4 +60,38 @@ router.post("/admin/user/role", Auth.requiredLogin, Auth.requiredSuperAdmin, (re
 });
 
 
+router.get("/admin/userCenter", Auth.requiredLogin, Auth.requiredAdmin, (req, res) => {
+	try {
+		res.render("userCenter", {
+			title: '个人中心',
+			localUser: res.locals.user
+		})
+	}catch(err) {
+		if (err) console.log(err);
+	}
+});
+
+router.post("/admin/user/modifyPassword", Auth.requiredLogin, Auth.requiredAdmin, (req, res) => {
+	try {
+		let _user = res.locals.user;
+		let password = req.body.password;
+		console.log(_user._id, password)
+		if (_user._id && password) {
+			User.findById(_user._id, (err, user) => {
+				user.password = password;
+				user.firstSave = true;
+				user.save((err, user) => {
+					if (err) console.log(err);
+					return res.json({
+						success: 1
+					})
+				})
+			})
+		}
+	}catch(err) {
+		console.log(err);
+	}
+})
+
+
 module.exports = router;
