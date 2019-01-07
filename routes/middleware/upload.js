1f80de976b1d5c49146557ba94529623e2f9531a
dyh-sjtu <dyh_sjtu@163.com>
2019-01-07 20:46:06 +0800

    fix

--- routes/middleware/upload.js
+++ routes/middleware/upload.js
@@ -12,7 +12,7 @@ exports.saveFile = (req, res, next) => {
 				let timestamp = Date.now();
 				let typeArr = originalFilename.split('.');
 				let type = typeArr[typeArr.length - 1];
-				let newFile = typeArr.slice(0, typeArr.length - 1).join("") + "_" + timestamp + '.' + type;
+				let newFile = typeArr.slice(0, typeArr.length - 1).join(".") + "_" + timestamp + '.' + type;
 				let newPath = path.join(__dirname, '../../', '/public/uploads/' + newFile);
 				fs.writeFile(newPath, data, (err, data) => {
 					req.newFile = "/uploads/" + newFile;
