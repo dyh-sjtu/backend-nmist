a60d300ac043af95a9d1b7c3bfd4317ae0609b6e
dyh-sjtu <dyh_sjtu@163.com>
2019-01-14 12:28:20 +0800

    fix

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+	name: String,
+	desc: String,
+	icon: String,
+	
+});
+
+ProductSchema.pre('save', function (next) {
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	} else {
+		this.meta.updateAt = Date.now();
+	}
+	next();
+});
+
+ProductSchema.statics = {
+	fetch: function (cb) {
+		return this
+			.find({})
+			.sort({'meta.updateAt': -1})
+			.exec(cb)
+	},
+	findById: function (id, cb) {
+		return this
+			.findOne({_id: id})
+			.exec(cb)
+	}
+};
+
+module.exports = mongoose.model('Product', ProductSchema);
